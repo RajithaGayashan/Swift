0b1080bfe718adf63886fdb7fc5a64ead8275f43
Rajitha Gayashan <rajithagayashan95@gmail.com>
2020-12-21 22:38:26 +0530

    Constant and Variables

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+var myVariable = 40
+myVariable = 50
+let myConstant = 40
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "The age is "
+let width = 25
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let quotation = """
+I said "I have \(apples) apples."
+And then I said "I have \(apples + oranges) pieces of fruit."
+"""
+
+
