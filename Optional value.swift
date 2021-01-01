73a033674b59855bb6dd2a3c964bc75c7337bcc3
Rajitha Gayashan <rajithagayashan95@gmail.com>
2021-01-01 08:59:13 +0530

    Optional value

---
+++ Optional value.swift
@@ -0,0 +1,11 @@
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+} else {
+    print("else path")
+}
